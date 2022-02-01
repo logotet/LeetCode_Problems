2f2963e22d53f42fab09583625a8ebd5d104b27b
logotet <vladimir.vasilev09@gmail.com>
2022-02-01 11:06:28 +0200

    Sudoku

--- src/Main.kt
+++ src/Main.kt
@@ -1,29 +1,73 @@
 import java.util.*
 
 fun main() {
-    val nums = intArrayOf(1)
-    val target = 1
-//  Input: nums = [5,7,7,8,8,10], target = 6
-//Output: [-1,-1]
-//Input: nums = [], target = 0
-//Output: [-1,-1]
+    val board = arrayOf(
+        arrayOf("5", "3", ".", ".", "7", ".", ".", ".", "."),
+        arrayOf("6", ".", ".", "1", "9", "5", ".", ".", "."),
+        arrayOf(".", "9", "8", ".", ".", ".", ".", "6", "."),
+        arrayOf("8", ".", ".", ".", "6", ".", ".", ".", "3"),
+        arrayOf("4", ".", ".", "8", ".", "3", ".", ".", "1"),
+        arrayOf("7", ".", ".", ".", "2", ".", ".", ".", "6"),
+        arrayOf(".", "6", ".", ".", ".", ".", "2", "8", "."),
+        arrayOf(".", ".", ".", "4", "1", "9", ".", ".", "5"),
+        arrayOf(".", ".", ".", ".", "8", ".", ".", "7", "9"),
+    )
+    var isvalid = true
 
-    var startPoint = -1
-    var endPoint = -1
-
-    for(i in nums.indices){
-        if(startPoint == -1 && target == nums[i]){
-            startPoint = i
+    //row check
+    for (row in board) {
+        val currentRow = mutableListOf<Char>()
+        var dotCounter = 0
+        for (i in row.indices) {
+            //TODO for the solution remove first()
+            if (row[i].first().isDigit()) {
+                currentRow.add(row[i].first())
+            } else {
+                dotCounter++
+            }
         }
-
-        if(i != startPoint && i != -1 && target == nums[i] && i > endPoint){
-            endPoint = i
+        if ((currentRow.distinct().size + dotCounter) != row.size) {
+            isvalid = false
         }
     }
 
-    if(startPoint != -1 && endPoint == -1){
-        endPoint = startPoint
+    //column check
+    for (i in 0..8) {
+        val currentColumn = mutableListOf<Char>()
+        var dotCounter = 0
+        for (y in 0..8) {
+            if (board[y][i].first().isDigit()) {
+                currentColumn.add(board[y][i].first())
+            } else {
+                dotCounter++
+            }
+        }
+        if ((currentColumn.distinct().size + dotCounter) != 9) {
+            isvalid = false
+        }
     }
-   print(Arrays.toString(intArrayOf(startPoint,endPoint)))
-//    return intArrayOf(startPoint, endPoint)
+
+    //cell check
+    for (i in 0..8 step 3) {
+        val cell = mutableListOf<Char>()
+        var dotCounter = 0
+        for (y in i..(i + 2)) {
+            cell.add(board[y][i].first())
+        }
+        println(cell)
+
+        0..2
+        0..2
+
+        0..2
+        3..5
+
+        0..2
+        6..8
+    }
+
+
+    print(isvalid)
+
+
 }
