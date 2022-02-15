6af375f0e8db6363b32f36b67b7fcb7061317980
logotet <vladimir.vasilev09@gmail.com>
2022-02-15 15:16:07 +0200

    45. Jump Game II resolved

--- src/medium/JumpGameII.kt
+++ src/medium/JumpGameII.kt
@@ -1,33 +1,32 @@
-//unresolved
 //No. 45
-fun main() {
-    val nums = intArrayOf(2,3,0,1,4)
-//    val nums = intArrayOf(
-//        10,9,8,7,6,5,4,3,2,1,1,0)
-    var count = 0
-    val finalIndex = nums.size - 1
-    var maxJumpPower = 0
+class JumpGameIIn {
+    fun jump(nums: IntArray): Int {
+        var maxJumpPower: Int
+        var numberOfJumps = 0
+        val finalPosition = nums.size - 1
 
-    var i = 0
-    outer@ while (i < finalIndex) {
-        maxJumpPower = nums[i]
-        if ((maxJumpPower + i) >= finalIndex) {
-            count++
-            break@outer
-        }
-        var strongestJumper = 0
-        var nextPosition = 0
-        for (y in i+1..i+maxJumpPower) {
-//              val closestPosition = (maxJumpPower + nums[y])
-            if(nums[y] >= strongestJumper){
-                strongestJumper = nums[y]
-                nextPosition = y
-
+        var i = 0
+        while (i < finalPosition) {
+            numberOfJumps++
+            maxJumpPower = nums[i]
+            val maxPosition = i + maxJumpPower
+            if (maxPosition >= finalPosition) {
+                break
             }
+            var maxCombinedPower = 0
+            var bestNextPosition: Int = 0
+            for (j in i + 1..maxPosition) {
+                if ((nums[j] + j) > maxCombinedPower) {
+                    maxCombinedPower = nums[j] + j
+                    bestNextPosition = j
+                }
+            }
+            i = bestNextPosition
         }
-        count++
-        i = nextPosition
-    }
 
-    print(count)
+        return numberOfJumps
+    }
 }
+//val nums = intArrayOf(2, 3, 1, 1, 4)
+//    val nums = intArrayOf(1,1,1,1,1)
+//    val nums = intArrayOf(10,9,8,7,6,5,4,3,2,1,1,0)
